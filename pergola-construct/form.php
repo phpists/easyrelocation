9fc7ecd6f7b617e26c77f8233defa9a72b0965bd
Urij <chopeyyuriy@gmail.com>
2023-12-07 13:46:28 +0200

    update forms v2

--- pergola-construct/form.php
+++ pergola-construct/form.php
@@ -52,18 +52,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET")
     $text = $_POST["message"] ?? null;
 
 
-    $message = "<b>Новая заявка</b> \n";
-    $message .= "Имя: <b>$name</b> \n";
-    $message .= "Фамилия: <b>$surname</b> \n";
-    $message .= "Телефон: <b>$phone</b> \n";
-    $message .= "E-mail: <b>$email</b> \n";
-    $message .= "Промокод: <b>$promo</b> \n";
-    $message .= "Вопрос: <b>$text</b> \n";
-    $message .= "Дата: " . "<b>" . date('Y-m-d H:i:s') . "</b>";
-
-
-    $telegram = new TelegramBotService();
-    $send = $telegram->sendMessage($message, ['parse_mode' => 'HTML']);
+//    $message = "<b>Новая заявка</b> \n";
+//    $message .= "Имя: <b>$name</b> \n";
+//    $message .= "Фамилия: <b>$surname</b> \n";
+//    $message .= "Телефон: <b>$phone</b> \n";
+//    $message .= "E-mail: <b>$email</b> \n";
+//    $message .= "Промокод: <b>$promo</b> \n";
+//    $message .= "Вопрос: <b>$text</b> \n";
+//    $message .= "Дата: " . "<b>" . date('Y-m-d H:i:s') . "</b>";
+//
+//
+//    $telegram = new TelegramBotService();
+//    $send = $telegram->sendMessage($message, ['parse_mode' => 'HTML']);
 
     $jsonData = json_encode(['status' => true]);
     header('Content-Type: application/json');
