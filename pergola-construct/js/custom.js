c12ae254a943a722d5a64f4646c5a2e5451607f8
Urij <chopeyyuriy@gmail.com>
2023-12-30 15:37:28 +0200

    altapatri add send message

--- pergola-construct/js/custom.js
+++ pergola-construct/js/custom.js
@@ -1,52 +1,57 @@
 $(document).ready(function () {
-  $(document).on("submit", ".form", function (e) {
-    e.preventDefault();
+    $(document).on("submit", ".form", function (e) {
+        e.preventDefault();
 
-    var form = $(this);
-    var data = form.serializeArray();
+        var form = $(this);
+        var data = form.serializeArray();
 
-    $.ajax({
-      url: "form.php",
-      data: data,
-      type: "POST",
-      dataType: "json",
-      success: function (response) {
-        var status = response.status;
-        if (status) {
-          $(".form_request").removeClass("show");
-          form[0].reset();
-          $(".success_modal").addClass("show");
-        }
-      },
+        $.ajax({
+            url: "form.php",
+            data: data,
+            type: "POST",
+            dataType: "json",
+            success: function (response) {
+                var status = response.status;
+                if (status) {
+                    $(".form_request").removeClass("show");
+                    form[0].reset();
+                    $(".success_modal").addClass("show");
+                }
+            },
+        });
     });
-  });
 
-  $(document).on("submit", ".fast_call", function (e) {
-    e.preventDefault();
+    $(document).on("submit", ".fast_call", function (e) {
+        e.preventDefault();
 
-    var form = $(this);
-    var data = form.serializeArray();
+        var form = $(this);
+        var data = form.serializeArray();
 
-    console.log("data", data);
+        console.log("data", data);
 
-    $.ajax({
-      url: "fast_call.php",
-      data: data,
-      type: "POST",
-      dataType: "json",
-      success: function (response) {
-        var status = response.status;
-        if (status) {
-          form[0].reset();
-          $(".success_modal").addClass("show");
+        $.ajax({
+            url: "fast_call.php",
+            data: data,
+            type: "POST",
+            dataType: "json",
+            success: function (response) {
+                var status = response.status;
+                if (status) {
+                    form[0].reset();
+                    $(".success_modal").addClass("show");
 
-          document
-            .querySelector(".success_modal .modal__close")
-            .addEventListener("click", () =>
-              $(".success_modal").removeClass("show")
-            );
-        }
-      },
+                    document
+                        .querySelector(".success_modal .modal__close")
+                        .addEventListener("click", () =>
+                            $(".success_modal").removeClass("show")
+                        );
+                }
+            },
+        });
     });
-  });
+
+    $(document).on('click', '.modal__close', function (e) {
+        $('.success_modal').removeClass('show');
+        $('body').css('overflow', '');
+    });
 });
