33cd33df1c00cdb528857e3624d063cd81f56b57
Yura <phpists@gmail.com>
2024-01-12 15:40:11 +0200

    dump db

--- easy-relocation/easyrelocation.sql
+++ easy-relocation/easyrelocation.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Час створення: Січ 10 2024 р., 23:09
--- Версія сервера: 10.4.26-MariaDB
--- Версія PHP: 8.1.9
+-- Хост: localhost
+-- Час створення: Січ 12 2024 р., 13:38
+-- Версія сервера: 8.0.32
+-- Версія PHP: 7.2.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База даних: `easyrelocation`
+-- База даних: `easyrelocation_db`
 --
 
 -- --------------------------------------------------------
@@ -28,14 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `site` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `surname` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `promo` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `ip` varchar(12) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int NOT NULL,
+  `site` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `surname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `promo` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ip` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `d_t` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -57,7 +57,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT для таблиці `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
