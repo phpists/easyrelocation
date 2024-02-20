1be1971610a4a6e04edf1118f287f4856cf7c0d0
Urij <chopeyyuriy@gmail.com>
2024-02-20 13:42:36 +0200

    fix forms

--- toursphuket/form.php
+++ toursphuket/form.php
@@ -25,12 +25,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET")
     $telegram = new TelegramBotService();
     $send = $telegram->sendMessage($message, ['parse_mode' => 'HTML']);
 
+// Database -------------------------------------------------------------------//
+    $servername = "localhost";
+    $username = "easyrelocation_db";
+    $password = "2D55rAgk3c";
+    $dbname = "easyrelocation_db";
+
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $dbname = "easyrelocation";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO orders (site, name, surname, phone, email, promo, ip, d_t)
+                VALUES ('" . $site . "', '" . $name . "', '" . '' . "', '" . $phone . "', '" . '' . "', '" . $callback . "', '" . $ip . "', '" . date('Y-m-d H:i:s') . "')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        //echo "New record created successfully";
+    } catch (PDOException $e) {
+        //echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+// Database -------------------------------------------------------------------//
 
 
     $jsonData = json_encode(['status' => true]);
     header('Content-Type: application/json');
-
-    header('Location: '. 'https://toursphuket.ru');
+    header('Location: ' . 'https://toursphuket.ru');
 }
 
 
